20f08b728cbb2041bf09fcb6d87a46c09f43bb32
roboticforest <roboticforest@gmail.com>
2020-11-11 23:33:52 -0800

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @author David Vitez (AKA: Robotic Forest)
+ * @copyright All rights reserved © 2020 David Vitez
+ *
+ * @brief Program Entry Point
+ * @details This is a test program for a number of tools that load and save Portable Pixmap files.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Portable Pixmap World!" << std::endl;
+}
